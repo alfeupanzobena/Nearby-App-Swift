c808460a4d767d85e37f6cae9f5733c33d6c4970
alfeupanzobena <alfeupanzobena28@gmail.com>
2024-12-11 01:01:00 +0100

    Initial Commit

---
+++ Nearby-App-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nearby-App-Swift
+//
+//  Created by Alfeu Panzo Bena on 11/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
